6cbe0231bedbaa6a22e91a8a4a45ec38cacff4d6
Luis Ramirez <luisjrm836@gmail.com>
2022-05-02 15:58:02 -0500

    [ADD] courses

--- src/migration/1636822567228-CreateCourses.ts
+++ src/migration/1636822567228-CreateCourses.ts
@@ -4,12 +4,11 @@ import {Course} from "../entity/Course";
 export class CreateCourses1636822567228 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const courses = [
-            { name: "Matematicas" },
-            { name: "Ingles" },
-            { name: "Religión" },
-            { name: "Naturales" },
-        ]
+        const coursesString : string = 'Teoría de Conjuntos,Aritmética,Algebra booleana,informática,matemáticas,Tecnogeometría,Humanidades,Lengua Castellana,Idioma Extranjero,Ciencias Naturales y Educación Ambiental,Ciencias Sociales,Educación Artística,Proyecto de vida,Educación Física,Recreación y Deportes,Tecnología e Informática';
+        const courses = coursesString.split(',').map(s => {
+            return { name: s}
+        })
+
         const repository = getRepository(Course);
         await repository.save(courses);
     }
